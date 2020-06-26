948b748fb7822431c8eaffd946bc9e3676451e24
Ahmad Awais <me@AhmadAwais.com>
2020-06-26 16:02:12 +0500

    📦 NEW: First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Cli Alerts.
+ *
+ * Cross platform CLI Alerts with colors &amp; colored symbols for success, info, warning, error. Work on macOS, Linux, and Windows.
+ *
+ * @author Awais <https://twitter.com/MrAhmadAwais/>
+ */
+module.exports = () => {};
---
+++ test.js
@@ -0,0 +1,3 @@
+const cliAlerts = require('./index.js');
+
+cliAlerts();
