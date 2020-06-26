68b13adc1c6d8f1e966be32b337a4cca0a53d2ef
Ahmad Awais <me@AhmadAwais.com>
2020-06-26 17:53:42 +0500

    📦 NEW: First version

---
+++ test.js
@@ -0,0 +1,8 @@
+const alert = require('./index.js');
+
+alert();
+alert({type: `success`, msg: `Everything finished!`, name: `DONE`});
+alert({type: `success`, msg: `Everything finished!`});
+alert({type: `warning`, msg: `You didn't add something!`});
+alert({type: `info`, msg: `Awais is awesome!`});
+alert({type: `error`, msg: `Something went wrong!`});
