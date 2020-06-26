eec18a44f1875d20b9482136dda91749590f4819
Ahmad Awais <me@AhmadAwais.com>
2020-06-26 18:13:35 +0500

    👌 IMPROVE: Screeshot or didn't happen

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const alert = require('./index.js');
 
-alert();
-alert({type: `success`, msg: `Everything finished!`});
-alert({type: `success`, msg: `Everything finished!`, name: `DONE`});
-alert({type: `warning`, msg: `You didn't add something!`});
+console.clear();
+
 alert({type: `info`, msg: `Awais is awesome!`});
 alert({type: `error`, msg: `Something went wrong!`});
+alert({type: `warning`, msg: `cli-alerts are cool!`});
+alert({type: `success`, msg: `Everything finished!`});
+alert({type: `success`, msg: `Everything finished!`, name: `DONE`});
+alert();
